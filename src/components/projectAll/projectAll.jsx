34e67f9f19ba5ca25e4e9f0d2a48db73e70943b1
anthopy <anthonymenghi@protonmail.com>
2023-05-23 21:01:42 +0200

    add: navbar

--- src/components/projectAll/projectAll.jsx
+++ src/components/projectAll/projectAll.jsx
@@ -39,29 +39,41 @@ const ProjectAll = ({ data }) => {
 
     return (
         <div>
-            <input type="text" value={searchTerm} onChange={handleSearchChange} placeholder="Search" />
-            <div>
+
+
+            <div className='pl-[5vw] pr-[5vw] mt-12'>
+                <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                <div class="relative">
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                    </div>
+                    <input value={searchTerm} onChange={handleSearchChange} type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." />
+                </div>
+            </div>
+
+
+            <div className="pl-[5vw] pr-[5vw] mt-4">
                 <button
-                    className={selectedcompetence.includes('Comprendre') ? 'btn btn-primary' : 'btn'}
+                    className={selectedcompetence.includes('Comprendre') ? 'btn mr-2 btn-primary' : 'btn mr-2'}
                     onClick={() => handlecompetenceChange('Comprendre')}
                 >
                     Comprendre
                 </button>
                 <button
-                    className={selectedcompetence.includes('Développer') ? 'btn btn-primary' : 'btn'}
+                    className={selectedcompetence.includes('Développer') ? 'btn btn-primary mr-2' : 'btn mr-2'}
                     onClick={() => handlecompetenceChange('Développer')}
                 >
                     Développer
                 </button>
                 <button
-                    className={selectedcompetence.includes('Exprimer') ? 'btn btn-primary' : 'btn'}
+                    className={selectedcompetence.includes('Exprimer') ? 'btn btn-primary mr-2' : 'btn mr-2'}
                     onClick={() => handlecompetenceChange('Exprimer')}
                 >
                     Exprimer
                 </button>
 
                 <button
-                    className={selectedcompetence.includes('Concevoir') ? 'btn btn-primary' : 'btn'}
+                    className={selectedcompetence.includes('Concevoir') ? 'btn btn-primary mr-2' : 'btn mr-2'}
                     onClick={() => handlecompetenceChange('Concevoir')}
                 >
                     Concevoir
