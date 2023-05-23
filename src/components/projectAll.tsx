b986fac38129a36f75d5de262134ef72a62070ca
anthopy <anthonymenghi@protonmail.com>
2023-05-23 16:54:00 +0200

    add : filter btn competence

--- src/components/projectAll.tsx
+++ src/components/projectAll.tsx
@@ -3,11 +3,32 @@ import styles from './projectAll.module.css';
 
 const ProjectAll = ({ data }) => {
     const [searchTerm, setSearchTerm] = useState('');
+    const [selectedcompetence, setSelectedcompetence] = useState([]);
+
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const handleSkillChange = (skill) => {
+        if (selectedcompetence.includes(skill)) {
+            setSelectedcompetence(selectedcompetence.filter((s) => s !== skill));
+        } else {
+            setSelectedcompetence([...selectedcompetence, skill]);
+        }
+    };
 
     const filteredData = data.filter((post) => {
         const titleMatch = post.frontmatter.title.toLowerCase().includes(searchTerm.toLowerCase());
         const descriptionMatch = post.frontmatter.description.toLowerCase().includes(searchTerm.toLowerCase());
-        return titleMatch || descriptionMatch;
+
+        if (selectedcompetence.length === 0) {
+            return titleMatch || descriptionMatch;
+        } else {
+            return (
+                (titleMatch || descriptionMatch) &&
+                selectedcompetence.some((skill) => post.frontmatter.competence.includes(skill))
+            );
+        }
     });
 
     const sortedData = filteredData.sort((a, b) => {
@@ -16,23 +37,36 @@ const ProjectAll = ({ data }) => {
         return dateB - dateA; // Sort in descending order (most recent first)
     });
 
-    const handleSearchChange = (event) => {
-        setSearchTerm(event.target.value);
-    };
-
     return (
         <div>
+            <input type="text" value={searchTerm} onChange={handleSearchChange} placeholder="Search" />
+            <div>
+                <button
+                    className={selectedcompetence.includes('Comprendre') ? 'btn btn-primary' : 'btn'}
+                    onClick={() => handleSkillChange('Comprendre')}
+                >
+                    Comprendre
+                </button>
+                <button
+                    className={selectedcompetence.includes('Développer') ? 'btn btn-primary' : 'btn'}
+                    onClick={() => handleSkillChange('Développer')}
+                >
+                    Développer
+                </button>
+                <button
+                    className={selectedcompetence.includes('Exprimer') ? 'btn btn-primary' : 'btn'}
+                    onClick={() => handleSkillChange('Exprimer')}
+                >
+                    Exprimer
+                </button>
 
-            <div className='pl-[5vw] pr-[5vw] mt-8'>
-                <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                <div class="relative">
-                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                    </div>
-                    <input value={searchTerm} onChange={handleSearchChange} type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search projects" required />
-                </div>
+                <button
+                    className={selectedcompetence.includes('Concevoir') ? 'btn btn-primary' : 'btn'}
+                    onClick={() => handleSkillChange('Concevoir')}
+                >
+                    Concevoir
+                </button>
             </div>
-
             <section className="pl-[5vw] pr-[5vw] flex justify-center flex-col width-full">
                 <div className={styles.projects_all}>
                     {sortedData.map((post) => (
